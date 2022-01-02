fb7eebb5a38ecedfdc171763f272dbfbb511a5f3
Afrose Hussain <76399407+afrosehussain@users.noreply.github.com>
2022-01-02 13:05:03 +0530

    first

---
+++ access_token.php
@@ -0,0 +1,48 @@
+<?php
+
+class Instamojo
+{
+    private $client_id;
+    private $client_secret;
+    private $url = "https://api.instamojo.com/oauth2/token/";
+    private $env = "production";
+
+    public function __construct($client_id, $client_secret)
+    {
+        $this->client_id = $client_id;
+        $this->client_secret = $client_secret;
+    }
+
+    public function getToken() {
+        if (substr( $this->client_id, 0, 5 ) === "test_") {
+            $this->url = "https://test.instamojo.com/oauth2/token/";
+            $this->env = "test";
+        }
+        $curl = curl_init($this->url);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, rawurldecode(http_build_query(array(
+            'client_id' => $this->client_id,
+            'client_secret' => $this->client_secret,
+            'grant_type' => 'client_credentials'
+        ))));
+        $json = json_decode(curl_exec($curl));
+        if(curl_error($curl))
+        {
+            echo 'error:' . curl_error($curl);
+        }
+        if (isset($json->error)) {
+            return "Error: " . $json->error;
+            throw new \Exception("Error: " . $json->error);
+        }
+        $this->token = $json;
+        return $this->env . $json->access_token;
+    }
+}
+
+$instamojo = new Instamojo("gkwVhdo9tZIx4On4EjnwzO5p0Pl2YmJELmVoTqdl", "X7bYePBAYyfFjrIIlXq8fBkSHakabnn2F0J3eQJZihZtgy2rMifXKlddTu3V8VZJZ5PPc5vzozRVMP98fcpPVSMei8F4B4rCTK7bOHS4zDxUzwYn9qZcksLY2J2B0xZz");
+
+echo $instamojo->getToken();
+?>
